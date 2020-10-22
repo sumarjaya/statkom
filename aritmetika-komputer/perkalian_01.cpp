e8be91501acde15e425c52b7ac83a9fc8b24b454
I Wayan Sumarjaya <42401245+sumarjaya@users.noreply.github.com>
2020-10-22 23:53:08 +0700

    Update perkalian_01.cpp

--- aritmetika-komputer/perkalian_01.cpp
+++ aritmetika-komputer/perkalian_01.cpp
@@ -1,7 +1,10 @@
-// perkalian.cpp untuk melihat perkalian
-// 111.111.111 x 111.111.111 dalam C++
-// compile dengan: g++ -std=c++2a -Wall -Werror -Og perkalian_01.cpp -o perkalian_01.exe
-// atau g++ -std=c++2a -Wall -Werror -Og perkalian_01.cpp -o perkalian_01.exe
+// perkalian.cpp adalah program C++ untuk melihat perkalian
+// 111.111.111 x 111.111.111 (bilangan yang cukup besar). Tujuan program 
+// ini adalah untuk mengamati lebih lanjut sifat-sifat titik kambang (floating point). 
+// Program ini dapat di-compile dengan compiler GNU GCC, Clang atau compiler
+// lain yang mendukung C++20. Lakukan:
+// pada GCC: g++ -std=c++2a -Wall -Werror -Og perkalian_01.cpp -o perkalian_01.exe
+// atau pada Clang: clang++ -std=c++2a -Wall -Werror -Og perkalian_01.cpp -o perkalian_01.exe
 #include <iostream>
 
 int main()
@@ -19,3 +22,4 @@ int main()
 // D:\workdir\statkom\CPP>clang++ -std=c++2a -Wall -Werror -Og perkalian_01.cpp -o perkalian_01.exe
 // D:\workdir\statkom\CPP>perkalian_01
 // Hasil kali: 111111111 kali 111111111 adalah 12345678987654321
+// Hasil ini eksak.
